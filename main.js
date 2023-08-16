78e66bfd8c974b2432d47c910d639d86e780933f
Михаил Малецков <97849486+mmaletskov@users.noreply.github.com>
2023-08-16 18:08:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.bike-slider,.slider__items').slick({
+        arrows:false,
+        dots:true,
+        fade:true, /* Чтобы картинки переключались плавно и без прокрутки. Эффект появления */
+        autoplay:true, /* Автообновление картинок */
+        autoplaySpeed:2000, /* Скорость обновления картинок  */
+
+    })
+})
